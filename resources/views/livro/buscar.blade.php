7d2b8f341659c1ec953be590a3818affddb0d33c
Ludson Almeida <ludson.bsa@gmail.com>
2018-06-14 10:07:33 -0300

    Webnario Kits

--- resources/views/livro/buscar.blade.php
+++ resources/views/livro/buscar.blade.php
@@ -1,28 +1,28 @@
 @extends(layout())
 
 @section('content')
-<section class="content" style="background:url('/images/bglead.jpg') repeat-x #f0f0f0;">
+    <section class="content" style="background:url('/images/bglead.jpg') repeat-x #f0f0f0;">
 
-    <h1 style="font-size:25px; font-weight: bold; color:#636363; margin-bottom:20px;">Buscar Livro</h1>
+        <h1 style="font-size:25px; font-weight: bold; color:#636363; margin-bottom:20px;">Buscar Kit Webnário</h1>
 
-    <section class="widget" style=" height:160px;">
+        <section class="widget" style=" height:160px;">
 
-        <div class="faixa"></div>
+            <div class="faixa"></div>
 
-        <br>
+            <br>
 
-        <form action="{{route('admin.livro.buscar-livro')}}" name="encontrarBrinde" id="brindeFind" method="post">
-            {!! csrf_field() !!}
-            <div class="field-wrap t96">
-                <label>Digite o E-mail:</label>
-                <br>
-                <input type="search" required id="buscaBr" class="t96" style="padding:12px;" name="buscarBrinde" placeholder="Digite o e-mail ou cep do contato...">
-            </div>
-        </form>
+            <form action="{{route('admin.livro.buscar-livro')}}" name="encontrarBrinde" id="brindeFind" method="post">
+                {!! csrf_field() !!}
+                <div class="field-wrap t96">
+                    <label>Digite o E-mail:</label>
+                    <br>
+                    <input type="search" required id="buscaBr" class="t96" style="padding:12px;" name="buscarBrinde" placeholder="Digite o e-mail ou cep do contato...">
+                </div>
+            </form>
 
+        </section>
+        @if(session()->has('msg'))
+            <h1>{!! session('msg') !!}</h1>
+        @endif
     </section>
-    @if(session()->has('msg'))
-        <h1>{!! session('msg') !!}</h1>
-    @endif
-</section>
 @endsection
